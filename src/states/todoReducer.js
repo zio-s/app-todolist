6df9a16d0e18e39648d6a5701d0efd1156b89497
seming2 <popqr3@naver.com>
2024-11-26 14:34:13 +0900

    주석정리

--- src/states/todoReducer.js
+++ src/states/todoReducer.js
@@ -1,28 +1,28 @@
-import { v4 as uuidv4 } from "uuid"
+import { v4 as uuidv4 } from 'uuid';
 
-// 액션 타입 정의
 export const ADD_TODO = 'ADD_TODO';
 export const UPDATE_TODO = 'UPDATE_TODO';
 export const DELETE_TODO = 'DELETE_TODO';
 
 export const setTodos = (state, action) => {
-  // action = {type: 'UPDATE_TODO', payload: {id: 1}}
-  // state = [{id: 1, task: '할일', isDone: false}, {...}, {...}]
   switch (action.type) {
     case ADD_TODO:
-      return [{
-        id: uuidv4(),
-        isDone: false,
-        task: action.payload.task,
-        createDate: new Date().toLocaleDateString()
-      }, ...state]
+      return [
+        {
+          id: uuidv4(),
+          isDone: false,
+          task: action.payload.task,
+          createDate: new Date().toLocaleDateString(),
+        },
+        ...state,
+      ];
     case UPDATE_TODO:
       return state.map((todo) => {
-        return todo.id === action.payload.id ? {...todo, isDone: !todo.isDone} : todo
-      })
+        return todo.id === action.payload.id ? { ...todo, isDone: !todo.isDone } : todo;
+      });
     case DELETE_TODO:
-      return state.filter((todo) => todo.id !== action.payload.id)
+      return state.filter((todo) => todo.id !== action.payload.id);
     default:
       return state;
   }
-}
+};
